d7d1529188c1b6989f1b4fa2f52488e856e4a228
AbdinasirM <aabdi5520@gmail>
2023-09-04 21:09:23 -0500

    Complete phone number validator that use API and displays the result on GUI

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# phoneNumberLookup
-Phone number lookup
+# Phone Number Validator App
+
+The Phone Number Validator App is a simple Python application with a graphical user interface (GUI) that allows you to validate phone numbers using an external API. It provides a user-friendly way to check whether a given phone number is valid and retrieve information about it.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Python 3.x installed on your system.
+- [python-dotenv](https://pypi.org/project/python-dotenv/) library for managing environment variables.
+
+## Getting Started
+
+1. Clone this repository to your local machine or download the source code.
+
+2. Install the required dependencies using pip:
+
+pip install -r requirements.txt
+
+javascript
+
+3. Create a `.env` file in the same directory as your Python script (e.g., `phone_number_validator.py`). In the `.env` file, store your API access key like this:
+
+ACCESS_KEY=your_api_access_key_here
+
+javascript
+
+Replace `your_api_access_key_here` with your actual API access key.
+
+## Usage
+
+1. Run the Phone Number Validator App:
+
+python phone_number_validator.py
+
+less
+
+2. The app's GUI window will appear, allowing you to input the phone number, country code, and country format code.
+
+3. Input your phone number, country code, and country format code in the respective fields.
+
+4. Click the "Validate" button to initiate the validation process.
+
+5. If the phone number is valid, you will receive a success message displaying information about the phone number in a user-friendly paragraph format.
+
+6. If the phone number is invalid or if there is an issue with the API, you will receive an error message indicating that the validation failed. Please check your input and try again.
+
+7. You can click the "Refresh" button to clear the input fields and start a new validation.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+- [python-dotenv](https://pypi.org/project/python-dotenv/) for managing environment variables.
+- [requests](https://pypi.org/project/requests/) library for making HTTP requests.
+- [Tkinter](https://docs.python.org/3/library/tkinter.html) for the GUI.
+
+## Author
+
+[Your Name]
+[Your Contact Information]
--- phonenumberlookup.py
+++ phonenumberlookup.py
@@ -1,59 +1,95 @@
+import os
 import requests
+import tkinter as tk
+from tkinter import ttk, messagebox
+from dotenv import load_dotenv  # Import load_dotenv from python-dotenv
 
-#the function below calls the api and validate the phone number by taking the access key, number, country code and format
-def validate_phone_number(access_key, number, country_code, format):
-    base_url = "http://apilayer.net/api/validate"
+# Load environment variables from .env file
+load_dotenv()
 
-    # Prepare the query parameters
-    params = {
+# Function to validate the phone number
+def validate_phone_number():
+    # Read API key from environment variable
+    access_key = os.getenv("ACCESS_KEY")
+    if access_key is None:
+        messagebox.showerror(
+            "Error", "API access key not found. Please check your .env file.")
+        return
+
+    number = number_entry.get()
+    country_code = country_code_entry.get().upper()
+    format = country_format_code_entry.get()
+
+    # Make the API request
+    response = requests.get("http://apilayer.net/api/validate", params={
         "access_key": access_key,
         "number": number,
         "country_code": country_code,
         "format": format
-    }
-
-    # Make the API request
-    response = requests.get(base_url, params=params)
+    })
 
     # Check if the request was successful
     if response.status_code == 200:
         data = response.json()
-        return data
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
-        return None
-
-
-
-#ask the user to enter number,country code, and country formatted code
-numberInput = input("Enter a phone number: ")
-countryCode = input("Enter country code: ")
-countryFormatCode = input("Enter country format code: ")
-
-
-#get data function
-def getData(numberInput, country_code, countryFormatCode):
-    if __name__ == "__main__":
-        access_key = "38be72506a5bb292e42bd3cc81dcab86"
-        number = numberInput
-        country_code = countryCode.upper()
-        format = countryFormatCode
-        
-        #make a call to the validator fuction and save the result in a variable data
-        data = validate_phone_number(
-            access_key, number, country_code, format)
-        #loop through the data and print the key and value pairs        
+        # Format and display the result as a paragraph
+        result_message = "Phone Number Validation Result:\n\n"
         for key, value in data.items():
-            print(f"{key}: {value}")
-        return data
+            result_message += f"{key.capitalize()}: {value}\n"
+        messagebox.showinfo("Validation Success", result_message)
+    else:
+        # Display a user-friendly error message
+        messagebox.showerror(
+            "Validation Error", "Phone number validation failed. Please check your input and try again.")
+
+# Function to clear input fields
 
 
+def clear_input_fields():
+    number_entry.delete(0, tk.END)
+    country_code_entry.delete(0, tk.END)
+    country_format_code_entry.delete(0, tk.END)
 
 
-# print the result
-result = getData(numberInput, countryCode, countryFormatCode)
-def displayResult(result):
-     print (result)
+# Create a Tkinter window
+window = tk.Tk()
+window.title("Phone Number Validator")
 
+# Create a style for ttk widgets
+style = ttk.Style()
+style.configure("TButton", padding=10, font=("Helvetica", 14))
+style.configure("TEntry", padding=10, font=("Helvetica", 14))
 
-# displayResult(result)
+# Create input fields with placeholders using ttk
+number_label = ttk.Label(
+    window, text="Enter a phone number (e.g., +1234567890):")
+number_label.pack(pady=10)  # Add margin at the top
+number_entry = ttk.Entry(window)
+number_entry.pack(pady=5)  # Add margin at the bottom
+number_entry.config(width=30)  # Increase input field width
+
+country_code_label = ttk.Label(window, text="Enter country code (e.g., US):")
+country_code_label.pack(pady=10)  # Add margin at the top
+country_code_entry = ttk.Entry(window)
+country_code_entry.pack(pady=5)  # Add margin at the bottom
+country_code_entry.config(width=30)  # Increase input field width
+
+country_format_code_label = ttk.Label(
+    window, text="Enter country format code (e.g., +1):")
+country_format_code_label.pack(pady=10)  # Add margin at the top
+country_format_code_entry = ttk.Entry(window)
+country_format_code_entry.pack(pady=5)  # Add margin at the bottom
+country_format_code_entry.config(width=30)  # Increase input field width
+
+# Create a "Validate" button using ttk
+validate_button = ttk.Button(
+    window, text="Validate", command=validate_phone_number)
+validate_button.pack(pady=10)  # Add margin at the top
+validate_button.config(width=20)  # Increase button width
+
+# Create a "Refresh" button using ttk
+refresh_button = ttk.Button(window, text="Refresh", command=clear_input_fields)
+refresh_button.pack(pady=10)  # Add margin at the top
+refresh_button.config(width=20)  # Increase button width
+
+# Run the Tkinter main loop
+window.mainloop()
